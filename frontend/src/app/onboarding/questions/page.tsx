c8bda5469f8c8bdba34f4a805bc39763795436d6
luvelayn <ktotochra9@gmail.com>
2026-04-18 17:52:46 +0300

    feat: add adaptive

--- frontend/src/app/onboarding/questions/page.tsx
+++ frontend/src/app/onboarding/questions/page.tsx
@@ -2,7 +2,7 @@ import { OnboardingQuestionsForm } from "@/features/onboarding/ui/onboarding-que
 
 export default function OnboardingQuestionsPage() {
   return (
-    <main className="flex min-h-[100svh] justify-center px-4 py-6 min-[375px]:px-5 min-[375px]:py-8 sm:p-8">
+    <main className="flex min-h-[100svh] justify-center px-4 py-6 min-[375px]:px-5 min-[375px]:py-8 sm:p-8 lg:items-center lg:px-10 lg:py-12">
       <OnboardingQuestionsForm />
     </main>
   );
